28acffa3b88915c10bfa0533c8a845a0202899fa
Bill Garrett <garrett@astralfrontier.org>
2023-05-14 13:02:42 -0700

    Rewrite AOE tool to create elements via jQuery

--- static/megalos.js
+++ static/megalos.js
@@ -1,3 +1,78 @@
+const DIE_LABELS = ["Top Left", "Top Right", "Bottom Left", "Bottom Right"];
+
+function intializeHtml() {
+  // The left side column, holding the grid
+  const leftColumn = $(`<div class="column is-narrow" />`);
+  for (let row = 0; row < 4; row++) {
+    let rowHtml = $(`<div class="columns" />`);
+    for (let col = 0; col < 4; col++) {
+      rowHtml.append(
+        $(
+          `<div class="column is-narrow" id="aoe-4x4-r${row + 1}c${
+            col + 1
+          }"></div>`
+        )
+      );
+    }
+    leftColumn.append(rowHtml);
+  }
+
+  const diceRow = $(`<div class="columns" />`);
+  for (let i = 0; i < 4; i++) {
+    diceRow.append(
+      $(`
+<div class="column">
+  <div class="field">
+      <label class="label">${DIE_LABELS[i]}</label>
+      <div class="control">
+          <div class="select">
+              <select id="aoe-4x4-d${i + 1}" class="aoe-4x4-die" value="R">
+                  <option value="R">(Roll Randomly)</option>
+                  <option value="0">(Blank)</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6H">6 (Horizontal)</option>
+                  <option value="6V">6 (Vertical)</option>
+              </select>
+          </div>
+      </div>
+  </div>
+</div>
+    `)
+    );
+  }
+
+  const buttonRow = $(`
+<div class="columns">
+  <div class="column is-narrow">
+    <button class="is-primary button" id="aoe-4x4-roll">Roll</button>
+    <button class="is-warning button" id="aoe-4x4-reset">Reset</button>
+  </div>
+  <div class="column">
+    <div class="content">
+      <p>
+        Dice won't randomize when you click "Roll" unless set to "(Roll Randomly)".
+        This lets you designate some values as blank, or to specify values for some dice.
+        Click "Reset" to clear the map and set all dice to be random.
+      </p>
+    </div>
+  </div>
+</div>
+  `);
+
+  const rightColumn = $(`<div class="column" />`).append(diceRow, buttonRow);
+
+  const interactiveTool = $(`<div class="columns" />`).append(
+    leftColumn,
+    rightColumn
+  );
+
+  $("#interactive-tool-root").append(interactiveTool);
+}
+
 function initializeGrid() {
   for (let rows = 1; rows < 5; rows++) {
     for (let cols = 1; cols < 5; cols++) {
@@ -94,6 +169,7 @@ function randomValue() {
 }
 
 $(function () {
+  intializeHtml();
   initializeGrid();
 
   $(".aoe-4x4-die").on("change", function () {
